f2a3edd1d7b480c3818c1c75569321a525d60f8b
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-23 01:06:42 +0000

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,37 @@
 class Instructor
-
+  
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def pass_student(student, test_name)
+    BoatingTest.all.each do |test|
+      if test.student == student && test.test_name == test_name
+        test.test_status = "passed"
+        test
+      else 
+        return BoatingTest.new(student, test_name, "passed", self)
+      end 
+    end 
+  end
+  
+  def fail_student(student, test_name)
+    BoatingTest.all.each do |test|
+      if test.student == student && test.test_name == test_name
+        test.test_status = "failed"
+        test
+      else 
+        return BoatingTest.new(student, test_name, "failed", self)
+      end 
+    end 
+  end 
+  
 end
