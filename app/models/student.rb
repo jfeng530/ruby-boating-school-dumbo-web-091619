499cebeaf681f28228426f88f8306ce92735f86b
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-23 00:50:54 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,23 @@
 class Student
-
+  
+  attr_accessor :first_name
+  @@all = []
+  
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end 
+  
+  def add_boating_test(student, boat_test, test_status, instructor)
+    BoatingTest.new
+  end 
+  
+  def self.find_student(first_name)
+    Student.all.find {|student| student.first_name == first_name }
+  end
+  
+  def grade_percentage
+    
+  end 
+  
 end
