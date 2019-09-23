59213cc14566b9386df24925b553101b09c0d44b
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-23 00:53:14 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -12,8 +12,8 @@ class Student
     @@all
   end
   
-  def add_boating_test(student, test_name, test_status, instructor)
-    BoatingTest.new(student, test_name, test_status, instructor)
+  def add_boating_test(self, test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
   end 
   
   def self.find_student(first_name)
