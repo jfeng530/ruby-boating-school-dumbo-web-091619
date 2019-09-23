0293934dc2923bad019d13c8d3d16f7091c7a8cc
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-23 00:55:17 +0000

    Automatically backed up by Learn

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,17 @@
 class Instructor
-
+  
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def pass_student(student, test_name)
+  
 end
